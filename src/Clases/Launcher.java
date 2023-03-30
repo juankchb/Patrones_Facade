ad817ca0d0d5c129234c1016a442ee97b63dee77
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2023-03-30 12:12:21 +0000

    Se agrego el main

--- src/Clases/Launcher.java
+++ src/Clases/Launcher.java
@@ -1,6 +1,7 @@
 //Esta clase debe generar el .txt
 public class Launcher{
     public static void main(String[] args) {
-
+        FacadeWeb pag = new FacadeWeb();
+        System.out.print(pag.display());
     }
 }
