7d1e2fd32fd1933d09b32d49f1acd495d6a5eec5
juankchb <89666771+juankchb@users.noreply.github.com>
2023-03-30 03:21:50 +0000

    changes

---
+++ src/Clases/SeccionCiencias.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class SeccionCiencias extends Section {
+	  public SeccionCiencias(String name) {
+	        super(name);
+	    }
+	    
+	    public void addCiencia(Article article) {
+	        super.addArticle(article);
+	    }
+	}
